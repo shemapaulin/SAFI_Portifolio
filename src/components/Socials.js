3ccd4c44416acd703625ea6e82f157f8ba407f2b
shemapaulin <shema@post.com>
2024-03-12 11:31:08 -0700

    social medias

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -4,13 +4,13 @@ const Socials = () => {
   return <div className='hidden xl:flex ml-24'>
     <ul className='flex gap-x-5 '>
     <li className='text-white  hover:text-amber-900' >
-      <a href='/'><ImFacebook/></a>
+      <a href='https://www.facebook.com/erneste.safi'><ImFacebook/></a>
     </li>
     <li className='text-white  hover:text-amber-900' >
       <a href='/'><ImTwitter/></a>
     </li>
     <li className='text-white  hover:text-amber-900' >
-      <a href='/'><ImInstagram/></a>
+      <a href='https://www.instagram.com/safi.erneste/'><ImInstagram/></a>
     </li>
     <li className='text-white  hover:text-amber-900' >
       <a href='/'><ImYoutube/></a>
